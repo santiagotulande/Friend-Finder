195f07fd4b1ad2e476a02626489263f3a3ce6263
Santiago tulande <santiagotulande@gmail.com>
2019-03-12 18:07:14 -0500

    add links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 	{
 		name: 'scooby doo',
-		photo: '',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjOy-7S2_3gAhUq6IMKHW-6BE8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.facebook.com%2Fscoobydoo%2F&psig=AOvVaw2Evqj8siovGQhNC4ZDfyf2&ust=1552518330649514',
 		scores: [
 			'1',
 			'5',
@@ -17,7 +17,7 @@ var friendsArray = [
 	},
 	{
 		name: 'gardfield',
-		photo: '',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwirj_W_2_3gAhXo4IMKHf2hCksQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGarfield_(character)&psig=AOvVaw2fKoKtAyDn2O8m7aoZU47D&ust=1552518297790827',
 		scores: [
 			'2',
 			'4',
